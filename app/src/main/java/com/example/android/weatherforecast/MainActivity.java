e061f44ea962282564ef94ad53a555a61826de30
Igor-developer <igordocmail@gmail.com>
2021-03-20 21:05:03 +0200

    Добавлена иконка.

---
+++ app/src/main/java/com/example/android/weatherforecast/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.weatherforecast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//Занятие 12.
+//Создать приложение на основе API.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
